71e24c2d1a829879b6d2271702d5f8891485d6d1
ciscomalware <ciscomalware@gmail.com>
2021-02-26 15:20:54 +0500

    feat: implement functional in towelSort function

--- src/index.js
+++ src/index.js
@@ -1,6 +1,12 @@
 
 // You should implement your task here.
 
-module.exports = function towelSort (matrix) {
-  return [];
+module.exports = function towelSort (matrix = []) {
+  if (matrix.length === 0) return [];
+  return matrix.reduce((acc, el, ind) => {
+    if (ind % 2 === 1 && ind !== 0) {
+      return [...acc, ...el.sort((a, b) => b - a)];
+    }
+    return [...acc, ...el];
+  }, []);
 }
